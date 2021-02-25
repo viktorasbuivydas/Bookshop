45a62573d1f25093fe58b0999895bc261d8ec181
Viktoras Buivydas <viktorasss00@gmail.com>
2021-02-25 23:26:55 +0200

    optimized from 3 to 1 query

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,9 +17,10 @@ class HomeController extends Controller
 
     public function index()
     {
-        $approved_book_count = Book::isApproved()->count();
-        $pending_book_count = Book::isPending()->count();
-        $rejected_book_count = Book::isRejected()->count();
+        $books = Book::select('is_approved')->get();
+        $approved_book_count = $books->where('is_approved', 1)->count();
+        $pending_book_count = $books->where('is_approved', NULL)->count();
+        $rejected_book_count = $books->where('is_approved', 0)->count();
         return view('home', compact('approved_book_count', 'pending_book_count', 'rejected_book_count'));
     }
 }
