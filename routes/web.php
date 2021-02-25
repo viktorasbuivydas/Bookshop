d3ccafe6fdc2db4a0b537fd7d8ccfb1bfbeca19b
Viktoras Buivydas <viktorasss00@gmail.com>
2021-02-26 00:59:11 +0200

    updated web routing

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,18 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('books/{book}', [App\Http\Controllers\BookController::class, 'show'])->name('books.show');
 
 //books
-Route::group(['prefix' => 'user', 'as'=>'user.', 'middleware' => 'auth'], function(){
-    Route::resource('books', User\BookController::class);
-    Route::resource('settings', User\SettingController::class);
-    Route::resource('reviews', User\ReviewController::class);
-    Route::post('reports/{book}', [User\ReportController::class, 'store'])->name('reports.store');
+Route::group(['namespace' => 'User', 'prefix'=> 'user', 'as'=>'user.', 'middleware' => 'auth'], function(){
+    Route::resource('books', BookController::class);
+    Route::resource('settings', SettingController::class);
+    Route::resource('reviews', ReviewController::class);
+    Route::post('reports/{book}', [App\Http\Controllers\User\ReportController::class, 'store'])->name('reports.store');
 });
 
-Route::group(['prefix' => 'admin', 'as'=>'admin.', 'middleware' => 'checkRole:admin'], function(){
-    Route::resource('genres', Admin\GenreController::class);
-    Route::resource('authors', Admin\AuthorController::class);
-    Route::resource('reports', Admin\ReportController::class);
-    Route::resource('books', Admin\BookController::class);
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'as'=>'admin.', 'middleware' => 'checkRole:admin'], function(){
+    Route::resource('genres', GenreController::class);
+    Route::resource('authors', AuthorController::class);
+    Route::resource('reports', ReportController::class);
+    Route::resource('books', BookController::class);
     Route::post('approve/{id}/{is_approved}', [App\Http\Controllers\Admin\BookController::class, 'approve'])->name('books.approve');
     Route::get('pending', [App\Http\Controllers\Admin\BookController::class, 'pending'])->name('books.pending');
 
